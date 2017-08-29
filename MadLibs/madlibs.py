93c8cf9e8b34ee79ff3f87fd63d32fb7ee135537
Bernard Allotey <benard.allotey4@gmail.com>
2017-08-28 20:43:04 -0500

    Added the infinitely long story variable.

---
+++ MadLibs/madlibs.py
@@ -0,0 +1,18 @@
+"""This program is basically an madlibs game, nothing more and nothing less."""
+
+STORY = ("This morning I woke up and felt %s because %s was going"
+        " to finally %s over the big %s. On the other side of the %s"
+        " were many %s protesting to keep %s in stores. The crowd bega"
+        "n to %s to the rhythm of the %s, which made all of the %s ver"
+        "y %s. %s tried to %s into the sewers and found %s rats. Needi"
+        "ng help, %s quickly called %s. %s appeared and saved %s by fly"
+        "ing to %s and dropping %s into a puddle of %s. %s then fell "
+        "asleep and woke up in the year %s, in a world where %s ruled "
+        "the world.")
+
+def welcome():
+    print("Welcome to the Mad Libs game!")
+    print("I'm your host, killer keemstar, and let's get riiiiiiiiii" +
+          "iiiiiiiiiiiight into the libs!")
+
+print(STORY)
