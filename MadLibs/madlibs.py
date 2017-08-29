407dfb2900fea1c54583cf7dc90c4b20b984c432
Bernard Allotey <benard.allotey4@gmail.com>
2017-08-28 23:22:18 -0500

    Added some comments and removed some unclean code

--- MadLibs/madlibs.py
+++ MadLibs/madlibs.py
@@ -1,14 +1,16 @@
 """This program is basically an madlibs game, nothing more and nothing less."""
 
+# Here you actually have to replace %s with {} because we are using python 3
 STORY = ("This morning I woke up and felt {} because {} was going"
-        " to finally {} over the big {}. On the other side of the {}"
-        " were many {} protesting to keep {} in stores. The crowd bega"
-        "n to {} to the rhythm of the {}, which made all of the {} ver"
-        "y {}. {} tried to {} into the sewers and found {} rats. Needi"
-        "ng help, {} quickly called {}. {} appeared and saved {} by fly"
-        "ing to {} and dropping {} into a puddle of {}. {} then fell "
-        "asleep and woke up in the year {}, in a world where {} ruled "
-        "the world.")
+         " to finally {} over the big {}. On the other side of the {}"
+         " were many {} protesting to keep {} in stores. The crowd bega"
+         "n to {} to the rhythm of the {}, which made all of the {} ver"
+         "y {}. {} tried to {} into the sewers and found {} rats. Needi"
+         "ng help, {} quickly called {}. {} appeared and saved {} by fly"
+         "ing to {} and dropping {} into a puddle of {}. {} then fell "
+         "asleep and woke up in the year {}, in a world where {} ruled "
+         "the world.")
+
 
 def welcome():
     print("Welcome to the Mad Libs game!")
@@ -17,10 +19,10 @@ def welcome():
 
 
 def input_variables():
-    #Surprise! It's arrays again, lol
+    # Surprise! It's arrays again, lol
     libs = []
     name = input("Enter a name here!")
-    #I am currently inserting the values into the array here
+    # I am currently inserting the values into the array here
     print("Enter three adjectives, separated by the enter key.")
     adj1 = input("Adjective 1: ")
     adj2 = input("Adjective 2: ")
@@ -68,11 +70,14 @@ def input_variables():
     libs.insert(22, name)
     libs.insert(23, year)
     libs.insert(24, noun4)
-    libs.insert(25, '')
     return libs
+
 
 libs = input_variables()
 
-print(libs[18])
-
-print(STORY.format(libs[0], libs[1], libs[2], libs[3], libs[4], libs[5], libs[6], libs[7], libs[8], libs[9], libs[10], libs[11],libs[12], libs[13], libs[14], libs[15], libs[16],libs[17], libs[18], libs[19], libs[20], libs[21],libs[22], libs[23], libs[24]))
+# Switch the percent for a simple .format and it works beautifully!
+print(STORY.format(libs[0], libs[1], libs[2], libs[3], libs[4], libs[5],
+                   libs[6], libs[7], libs[8], libs[9], libs[10], libs[11],
+                   libs[12], libs[13], libs[14], libs[15], libs[16], libs[17],
+                   libs[18], libs[19], libs[20], libs[21], libs[22], libs[23],
+                   libs[24]))
