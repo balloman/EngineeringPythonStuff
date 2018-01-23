b5aa66f9c23ea400f3bf594c165ebe45498bd751
Bernard Allotey <benard.allotey4@gmail.com>
2018-01-23 14:30:35 -0600

    Added Project

---
+++ Note Converter/proj02.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+
+def welcomeMessage():
+    print "Hello."
+    print "This program will convert octave/pitchclass pairs into their respective frequency."
+
+def inputFunc():
+	octave = int(raw_input("Input an octave"))
+	pitchclass = int(raw_input("Input a pitch class"))
+
+	return octave, pitchclass
+
+def freq(infTup):
+
+
+def math(octa, pc):
+
